bde496577379b54059b98ce07938bd244dceec2d
YamanaMeghana <161587550+YamanaMeghana@users.noreply.github.com>
2024-05-15 17:24:06 +0530

    Update main.tf

--- scripts/main.tf
+++ scripts/main.tf
@@ -65,18 +65,18 @@ resource "aws_security_group" "project_securitygroup" {
   }
 }
 
-resource "tls_private_key" "tomcat" {
+resource "tls_private_key" "web_key" {
   algorithm = "RSA"
 }
 
-resource "aws_key_pair" "tomcat" {
-  key_name   = "tomcat"
-  public_key = tls_private_key.tomcat.public_key_openssh
+resource "aws_key_pair" "app_key" {
+  key_name   = "web-key"
+  public_key = tls_private_key.web_key.public_key_openssh
 }
 
-resource "local_file" "tomcat" {
-  content  = tls_private_key.tomcat.private_key_pem
-  filename = "tomcat.pem"
+resource "local_file" "web_key" {
+  content  = tls_private_key.web_key.private_key_pem
+  filename = "web-key.pem"
 
   provisioner "local-exec" {
     command = "chmod 600 ${self.filename}"
@@ -85,10 +85,10 @@ resource "local_file" "tomcat" {
 }
 
 resource "aws_instance" "kubernetes_master" {
-  ami             = "ami-0bb84b8ffd87024d8"
+  ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.subnet_1.id
-  key_name        = "tomcat"
+  key_name        = "web-key"
   security_groups = [aws_security_group.project_securitygroup.id]
   tags = {
     Name = "Kubernetes-Master"
@@ -100,23 +100,23 @@ resource "aws_instance" "kubernetes_master" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.tomcat.private_key_pem
+    private_key = tls_private_key.web_key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
         command = " echo ${aws_instance.kubernetes_master.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  	command = "ansible-playbook /var/lib/jenkins/workspace/MedicureHealthcare/scripts/k8s-master-setup.yml"
+  	command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8s-master-setup.yml"
   }
   
 }
 
 resource "aws_instance" "kubernetes_worker_1" {
-  ami             = "ami-0bb84b8ffd87024d8"
+  ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.subnet_1.id
-  key_name        = "tomcat"
+  key_name        = "web-key"
   security_groups = [aws_security_group.project_securitygroup.id]
   tags = {
     Name = "Kubernetes-Worker-1"
@@ -128,23 +128,23 @@ resource "aws_instance" "kubernetes_worker_1" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.tomcat.private_key_pem
+    private_key = tls_private_key.web_key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
         command = " echo ${aws_instance.kubernetes_worker_1.public_ip} > inventory "
   }
    provisioner "local-exec" {
-       command = "ansible-playbook /var/lib/jenkins/workspace/MedicureHealthcare/scripts/k8s-worker-setup.yml "
+       command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8s-worker-setup.yml "
   }
   depends_on = [aws_instance.kubernetes_master]
 }
 
 resource "aws_instance" "kubernetes_worker_2" {
-  ami             = "ami-0bb84b8ffd87024d8"
+  ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.subnet_1.id
-  key_name        = "tomcat"
+  key_name        = "web-key"
   security_groups = [aws_security_group.project_securitygroup.id]
   tags = {
     Name = "Kubernetes-Worker-2"
@@ -156,14 +156,14 @@ resource "aws_instance" "kubernetes_worker_2" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.tomcat.private_key_pem
+    private_key = tls_private_key.web_key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
         command = " echo ${aws_instance.kubernetes_worker_2.public_ip} > inventory "
   }
    provisioner "local-exec" {
-       command = "ansible-playbook /var/lib/jenkins/workspace/MedicureHealthcare/scripts/k8s-worker-setup.yml "
+       command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/k8s-worker-setup.yml "
   }
   depends_on = [aws_instance.kubernetes_worker_1]
 }
@@ -175,20 +175,56 @@ resource "null_resource" "local_command" {
   }
    
    provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/MedicureHealthcare/scripts/monitring-deployment.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitring-deployment.yml"
   }
 
    provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/MedicureHealthcare/scripts/deployservice.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/deployservice.yml"
   }
   depends_on = [aws_instance.kubernetes_worker_2]
 
 }
+
+resource "null_resource" "local_command" {
+  
+   provisioner "local-exec" {
+        command = " echo ${aws_instance.kubernetes_worker_1.public_ip} > inventory "
+  }
+   
+   provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitring-deployment.yml"
+  }
+
+   provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/deployservice.yml"
+  }
+  depends_on = [null_resource.local_command]
+
+}
+
+resource "null_resource" "local_command" {
+  
+   provisioner "local-exec" {
+        command = " echo ${aws_instance.kubernetes_worker_2.public_ip} > inventory "
+  }
+   
+   provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitring-deployment.yml"
+  }
+
+   provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/deployservice.yml"
+  }
+  depends_on = [aws_instance.kubernetes_worker_2]
+
+}
+
+
 resource "aws_instance" "monitoring_server" {
-  ami             = "ami-0bb84b8ffd87024d8"
+  ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.subnet_1.id
-  key_name        = "tomcat"
+  key_name        = "web-key"
   security_groups = [aws_security_group.project_securitygroup.id]
   tags = {
     Name = "Monitoring-Server"
@@ -200,14 +236,14 @@ resource "aws_instance" "monitoring_server" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = tls_private_key.tomcat.private_key_pem
+    private_key = tls_private_key.web_key.private_key_pem
     host        = self.public_ip
   }
    provisioner "local-exec" {
         command = " echo ${aws_instance.monitoring_server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/MedicureHealthcare/scripts/monitring.yml "
+  command = "ansible-playbook /var/lib/jenkins/workspace/Healthcare/scripts/monitring.yml "
   }
 depends_on = [null_resource.local_command]
   
